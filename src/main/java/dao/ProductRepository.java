3075ce9afa4ec61ff864e36d398772ae691fc9c1
sonyoungsik <ysyssom@hanmail.net>
2022-05-19 11:15:41 +0900

    productRepository db 형태로 저장

---
+++ src/main/java/dao/ProductRepository.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import dto.Product;
+
+
+public class ProductRepository {
+
+	private List<Product> products = new ArrayList<>();
+	
+	public ProductRepository() {
+		
+		Product phone = new Product("P1234", "iPhone6s", 800000);
+		phone.setDescription("4.7-inch, 1334X750 Retina HD display, 8-megapixel iSight Camera");
+		phone.setCategory("Smart Phone");
+		phone.setManufacturer("Apple");
+		phone.setUnitsInStock(1000);
+		phone.setCondition("New");
+		
+		Product notebook = new Product("P1235", "LG PC ±×·¥", 1500000);
+		notebook.setDescription("13.3-inch, IPS LED display, 5rd Generation Intel Core processors");
+		notebook.setCategory("Notebook");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+		
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display, Octa-Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("Apple");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
+		
+		products.add(phone);
+		products.add(notebook);
+		products.add(tablet);
+		
+	}
+	public List<Product> getAllProducts(){
+		return products;
+	}
+}
