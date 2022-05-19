9ed010c105e1726b40345e89433a410239ade07a
sonyoungsik <ysyssom@hanmail.net>
2022-05-19 10:24:50 +0900

    JSP 기본 모델 작성(초기)

---
+++ src/main/java/dto/Product.java
@@ -0,0 +1,91 @@
+package dto;
+
+// 모델 클래스 작성
+// JSP : 자바빈즈 (getter / setter, 기본 생성자(필드 없는거), Serializable 등 생성 필수)
+
+public class Product {
+
+	private String productId;
+	private String name;
+	private int unitPrice;
+	private String description;
+	private String manufacturer;
+	private String category;
+	private long unitsInStock;
+	private String condition;
+	
+	public Product() {
+		
+	}
+
+	public Product(String productId, String name, int unitPrice) {
+		this.productId = productId;
+		this.name = name;
+		this.unitPrice = unitPrice;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public long getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(long unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+}
